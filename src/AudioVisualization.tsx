36006a511629fb1d4b43539d37bdf461cbf71c7c
Vadim Avdeev <v.avdeiev@gmail.com>
2023-08-01 11:23:57 -0700

    make visualization width dynamic

--- src/AudioVisualization.tsx
+++ src/AudioVisualization.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useLayoutEffect, useState } from "react";
 import useAudioPeaks from "./hooks/useAudioPeaks";
 
 export default function AudioVisualization({
@@ -8,12 +8,15 @@ export default function AudioVisualization({
   audioData: AudioBuffer | null;
   progress: number;
 }) {
-  const width = 220;
-  const height = 16;
+  const svgRef = useRef<SVGSVGElement>();
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
   const barWidth = 2;
   const barGap = 1;
   const barCount = Math.round(width / (barWidth + barGap));
   const peaks = useAudioPeaks(audioData, barCount);
+
   const { current: clipPathId } = useRef<string>(
     Math.random().toString(36).substring(2)
   );
@@ -39,8 +42,31 @@ export default function AudioVisualization({
     return result;
   };
 
+  useLayoutEffect(() => {
+    const bbox = svgRef.current.getBoundingClientRect();
+    setWidth(bbox.width);
+    setHeight(bbox.height);
+
+    const observer = new ResizeObserver((entries) => {
+      const entry = entries[0];
+      let w, h;
+      if (entry.borderBoxSize) {
+        w = entry.borderBoxSize[0].inlineSize;
+        h = entry.borderBoxSize[0].blockSize;
+      } else {
+        w = entry.contentRect.width;
+        h = entry.contentRect.height;
+      }
+
+      setWidth(w);
+      setHeight(h);
+    });
+    observer.observe(svgRef.current);
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <svg className="rvmp-viz-container" width={width} height={height}>
+    <svg className="rvmp-viz-container" ref={svgRef}>
       <defs>
         <clipPath id={clipPathId}>{renderBars()}</clipPath>
       </defs>
