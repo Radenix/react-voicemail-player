dd8a064dc96b2896f404b913a5275f8ae03501e0
Vadim Avdeev <v.avdeiev@gmail.com>
2023-08-26 17:06:52 -0700

    configure api-extractor
    Install @microsoft/api-extractor package and configure it to do a .d.ts rollup, i.e. merging all exported types into a single  file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export * from "./VoicemailPlayer";
+/**
+ * Audio player component for React that displays audio amplitude peaks
+ * @packageDocumentation
+ */
+
+export { default } from "./VoicemailPlayer";
+export type { VoicemailPlayerProps } from "./VoicemailPlayer";
